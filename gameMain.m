4b35836727e68f707d1ecdae2ddd769803fa223d
rutisersundar1 <rutisersundar.1@buckeyemail.osu.edu>
2021-11-12 15:25:03 -0500

    Laid out some main files
    -Created gameMain, which takes care of the physics and main computation
    -Removed most things from rocket_physics which is going to be deleted.

--- gameMain.m
+++ gameMain.m
@@ -1,25 +1,90 @@
-%Display and interface code for FES21.
-%Should manage inputs from user
-%Probably don't use the simpleGameEngine key input methods, as they pause
-%execution of the entire program to wait for input.
-%OOh matlab has a nice way to do things
+%FARM EQUIPMENT SIMULATOR 2021
+%BUY NOW
 
-%key definitions
-%a = 97
-%d = 100
-%s = 73
-%w = 119
+clear
+clc
+
+%%
+%--------VALUES--------
+%Changing rocket physics values
+acceleration = [0,0]; %xy acceleration, meters per second squared.
+velocity = [0,0]; %xy velocity, meters per second
+position = [0,0]; %should not change since the rocket will be centered
+rotation = 0; %degrees, positive is clockwise
+throttle = 0; %0 to 1
+mass = 0; %mass, kilograms
+altitude = 0; %meters above sea level
+prop_mass = 0; %mass of propellant in kilograms
+engine_force = [0,0]; %engine force in newtons
+gravity_force = [0,0]; %force of gravity in newtons
+
+%--------CONSTANTS--------
+throttle_inc = 0.1; %per frame
+rotation_inc = 5; %degrees per frame
+
+%ROCKET CONSTANTS
+gravity = 9.806; %gravity, meters per second squared
+dry_mass = 100000; %dry mass, kilograms
+starting_prop_mass = 300000; %default prop mass, kilograms
+isp = 350; %specific impulse, seconds
+
+%COW CONSTANTS
+cow_prop_mass = 1000; %mass of propellant given by cow, kilograms
+cow_avg_dist = 1000; %average distance between cows, meters
+cow_rand_max = 100; %maximum deviation from this average distance, meters
+
+%%
+%INITIALIZE GAME
 
 keybuffer = keyBuffer(); %Buffer buffered keys with a key buffer.
 
 game_figure = figure('Color', 'blue'); %Initialize the game figure.
 set(game_figure, 'KeyPressFcn', @bufferKeys); %Assign the key buffer to handle key presses.
 
-gameState = "paused";
+gameState = "paused"; %game state
+prop_mass = starting_prop_mass;
+mass = prop_mass + dry_mass;
+altitude = 0;
+rotation = 270;
 
-while gameState ~= "paused"
+%% 
+%RUN GAME
+
+while gameState ~= "crashed"
+    frameStart = tic(); %start frame timer
     throttleinputs = getThrottleInput(keybuffer);
     rotInput = getRotInput(keybuffer);
+    
+    %Handle throttle inputs. z is max. x is cut.
+    %w is increase, s is decrease
+    switch(throttleinputs)
+        case "z"
+            throttle = 1;
+        case "x" 
+            throttle = 0;
+        case "w" 
+            throttle = throttle + throttle_inc * toc(frameStart);
+        case "s"
+            throttle = throttle - throttle_inc * toc(frameStart);
+    end
+    
+    %Handle rotation inputs. Right is d and left is a.
+    switch(rotInput)
+        case "d"
+            rotation = rotation + rotation_inc * toc(frameStart);
+        case "a"
+            rotation = rotation - rotation_inc * toc(frameStart);
+    end
+   
+    %Constrain rotation between 0 and 360 by adding or subtracting 360.
+    while rotation > 360
+        rotation = rotation - 360;
+    end
+    
+    while rotation < 0
+        rotation = rotation + 360;
+    end
+    
 end
 
 %Just because I can't pass arguments in through a callback. Sigh.
--- rocket_physics.m
+++ rocket_physics.m
@@ -1,55 +1,24 @@
 
 %% FARMING EQUIPMENT SIMULATOR 3021
 %% A GAME ABOUT FARMING WITH... HEAVIER EQUIPMENT
-clear
-clc
-
 %% 
-%CONSTANTS
-orbit_altitude_required = 100000; %karman line, meters
-orbit_horiz_speed_required = 7800; %orbit speed meters per second. might be used
 
-cow_prop_mass = 1000; %mass of propellant given by cow, kilograms
-cow_avg_dist = 1000; %average distance between cows, meters
-cow_rand_max = 100; %maximum deviation from this average distance, meters
+%CONSTANTS
+%orbit_altitude_required = 100000; %karman line, meters
+%orbit_horiz_speed_required = 7800; %orbit speed meters per second. might be used
+
 
 gravity = 9.806; %gravity, meters per second squared
 dry_mass = 100000; %dry mass, kilograms
 starting_prop_mass = 300000; %default prop mass, kilograms
 isp = 350; %specific impulse, seconds
 
-%keys
-pause_key = "esc";
-rotate_right_key = "d";
-rotate_left_key = "a";
-throttle_inc_key = "shift";
-throttle_dec_key = "ctrl";
-throttle_max_key = "z";
-throttle_cut_key = "x";
-
 %%
 %CHANGING VALUES
 points = 0; %game score
 
-%Rocket physics values
-acceleration = [0,0]; %xy acceleration, meters per second squared.
-velocity = [0,0]; %xy velocity, meters per second
-position = [0,0]; %should not change since the rocket will be centered
-rotation = 0; %degrees, positive is counterclockwise
-throttle = 0; %0 to 1
-altitude = 0; %meters above sea level
-rocket_mass = 0; %kilograms
-prop_mass = 0;
-
 engine_force = [0,0];
 gravity_force = [0,0];
-
-game_state = "prelaunch";
-%"prelaunch"
-%"flight"
-%"crashed"
-%"paused"
-%"orbit"
 
 %% 
 %INITIALIZE GAME
