630c82829bdd2f5542388f5e54033082aefef0ae
rutisersundar1 <rutisersundar.1@buckeyemail.osu.edu>
2021-11-18 14:11:31 -0500

    Show the physics!
    The rocket can now fly off screen very fast!

--- gameMain.m
+++ gameMain.m
@@ -9,7 +9,7 @@ clc
 %Changing rocket physics values
 acceleration = [0,0]; %xy acceleration, meters per second squared.
 velocity = [0,0]; %xy velocity, meters per second
-position = [0,0]; %should not change since the rocket will be centered
+position = [0,0.5]; %should not change since the rocket will be centered
 rotation = 0; %degrees, positive is clockwise
 throttle = 0; %0 to 1
 mass = 0; %mass, kilograms
@@ -19,10 +19,11 @@ propConsumed = 0; %propellant consumed each frame, kilograms
 thrust_s = 0; %scalar thrust, newtons
 thrust_v = [0,0]; %vector thrust, newtons
 gravityForce = [0,0]; %force of gravity, newtons
-delta_x = [0,0]; %change in position each frame, meters
+delta_pos = [0,0]; %change in position each frame, meters
 frictionMultiplier = 0.98; %velocity is multiplied by this each frame to approximate friction
 frameRate = 60; %frames per second
 frameTime = 1/frameRate; %seconds
+metersToPixels = 2; %meters per pixel
 
 %--------CONSTANTS--------
 throttleInc = 0.1; %per second
@@ -30,10 +31,10 @@ rotationInc = 180; %degrees per second
 
 %ROCKET CONSTANTS
 gravity = -9.806; %gravity, meters per second squared
-dryMass = 100000; %dry mass, kilograms
+dryMass = 10000; %dry mass, kilograms
 startingPropMass = 300000; %default prop mass, kilograms
 fuelRate = 100; %propellant burned per second at maximum throttle, kilograms per second
-maxThrust = 200000; %maximum thrust, newtons
+maxThrust = 20000000; %maximum thrust, newtons
 
 %COW CONSTANTS
 cowPropMass = 1000; %mass of propellant given by cow, kilograms
@@ -43,12 +44,29 @@ cowRandMax = 100; %maximum deviation from this average distance, meters
 %%
 %INITIALIZE GAME
 
+%Game States
+%1 = running
+%0 = paused
+%-1 = close (by x button)
 gameFigure = figure('Color', 'blue'); %Initialize the game figure.
 gameFigure.UserData = struct('bufferedRot', 0, 'bufferedThrottle', 0,...
-    'score', 0);
-set(gameFigure, 'KeyPressFcn', @bufferKeys); %Assign the key buffering function
+    'score', 0, 'gameState', 1);
 
-gameState = "paused"; %game state
+%Key buffering function to handle key presses
+set(gameFigure, 'KeyPressFcn', @bufferKeys);
+
+pause(0.00001);
+%This function runs when the user closes the window through the X button.
+%It will print the user's score, close the window, and stop the game.
+%set(gameFigure, 'CloseRequestFcn', @closeRequestMgr); 
+pause(0.01);
+
+%Set the figure's name
+set(gameFigure, 'Name', 'Farm Equipment Simulator 2021');
+%Go fullscreen
+set(gameFigure, 'WindowState', 'maximized');
+pause(0.01);
+
 propMass = startingPropMass;
 mass = propMass + dryMass;
 altitude = 0;
@@ -64,54 +82,69 @@ rocketAlpha = imread('assets/RocketAlpha.png'); %rocket alpha because MatLab tra
 %% 
 %RUN GAME
 
-while gameState ~= "crashed"
+while true
     elapsedTime = tic();
     
-    throttleInput = gameFigure.UserData.bufferedThrottle;
-    rotInput = gameFigure.UserData.bufferedRot;
+    %If the user has paused
+    if gameFigure.UserData.gameState == 0
+        
+    else if gameFigure.UserData.gameState == 1
+            %Take inputs from buffer
+            throttleInput = gameFigure.UserData.bufferedThrottle;
+            rotInput = gameFigure.UserData.bufferedRot;
+            
+            %fprintf("throttleInput: %i, rotInput, %i\n", throttleInput, rotInput);
+            
+            %Update throttle and rotation
+            throttle = calcThrottle(throttleInput, throttle, throttleInc, frameTime);
+            rotation = calcRotation(rotInput, rotation, rotationInc, frameTime);
+            
+            %Clear inputs so they aren't used again next frame
+            gameFigure.UserData.bufferedThrottle = 0;
+            gameFigure.UserData.bufferedRot = 0;
+            
+            %Calculate thrust
+            thrust_s = maxThrust * throttle; %scalar thrust value, Newtons
+            thrust_v = [thrust_s * sind(rotation), thrust_s * cosd(rotation)]; %vector thrust force, Newtons
+            
+            %amount of propellant consumed this frame
+            propConsumed = fuelRate * throttle * frameTime;
+            
+            %subtract consumed propellant and set mass
+            propMass = propMass - propConsumed; %kg
+            mass = propMass + dryMass; %kg
+            
+            %Calculate forces
+            gravityForce = [0, mass * gravity]; %force of gravity, Newtons
+            
+            netForce = thrust_v + gravityForce; %net force, Newtons
+            
+            %Apply acceleration
+            acceleration = netForce / mass; %acceleration, meters per second squared
+            
+            velocity = velocity + acceleration * frameTime; %meters per second
+            velocity = velocity * frictionMultiplier; %rough approximation of air drag
+            
+            delta_pos = velocity * frameTime; %movement this frame, meters
+            altitude = altitude + delta_pos(2); %increment altitude as necessary
+                        
+            position = position + delta_pos;
+            
+            %Move background and update scoring
+            %translateBg(Background, -1 * delta_x);
+            h = axes('position', [position(1), position(2), 0.4, 0.4]);
+            
+            rotatedRocketImg = imrotate(rocketImg, -rotation, 'nearest', 'loose');
+            imshow(rotatedRocketImg);
+            
+            axis off
+        else %Something went wrong otherwise.
+            delete(gameFigure);
+            return
+        end
+    end
     
-    fprintf("throttleInput: %i, rotInput, %i\n", throttleInput, rotInput);
-    
-    %Update throttle and rotation
-    throttle = calcThrottle(throttleInput, throttle, throttleInc, frameTime);
-    rotation = calcRotation(rotInput, rotation, rotationInc, frameTime);
-    
-    %Clear inputs so they aren't used again next frame
-    gameFigure.UserData.bufferedThrottle = 0;
-    gameFigure.UserData.bufferedRot = 0;
-    
-    %Calculate thrust
-    thrust_s = maxThrust * throttle; %scalar thrust value, Newtons
-    thrust_v = [thrust_s * cosd(rotation), thrust_s * sind(rotation)]; %vector thrust force, Newtons
-    
-    %amount of propellant consumed this frame
-    propConsumed = fuelRate * throttle * frameTime;
-
-    %subtract consumed propellant and set mass
-    propMass = propMass - propConsumed; %kg
-    mass = propMass + dryMass; %kg
-    
-    %Calculate forces
-    gravityForce = [0, mass * gravity]; %force of gravity, Newtons
-    
-    netForce = thrust_v + gravityForce; %net force, Newtons
-    
-    %Apply acceleration
-    acceleration = netForce / mass; %acceleration, meters per second squared
-    
-    velocity = velocity + acceleration * frameTime; %meters per second
-    velocity = velocity * frictionMultiplier; %rough approximation of air drag
-    
-    delta_x = velocity * frameTime; %movement this frame, meters
-    altitude = altitude + delta_x(2); %increment altitude as necessary
-    
-    %Move background and update scoring
-    %translateBg(Background, -1 * delta_x);
-    %scoreCounter(ScoreCounter, altitude, rotation, physicsFrameTime);
-    %score = getScore(ScoreCounter);
-
-    rotatedRocketImg = imrotate(rocketImg, -rotation, 'nearest', 'loose');
-    h = imshow(rotatedRocketImg);
+    drawnow
     
     %Wait so that it continues at the correct framerate
     if elapsedTime < frameTime
